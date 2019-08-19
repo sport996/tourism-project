2c25547009d0dae8e80ec20d230a3ab91401ef17
Ahmed <sport996@github.com>
2019-08-19 11:13:53 +0300

    Working image api testing

--- tourism-project/src/CityInfo.js
+++ tourism-project/src/CityInfo.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Link
-// } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import axios from 'axios';
 import CityImage from './CityImage';
 
@@ -22,13 +22,13 @@ export default class CityInfo extends Component {
   componentDidMount(){
       axios({
           method: "GET",
-          url: ""
+          url: "https://pixabay.com/api/?key=13352675-ccd96ebbc533b34c0a3a93225"
       })
       .then(response =>{
           console.log(response)
           this.setState({
-            name: response.data.xxxx,
-            image: response.data.xxxx
+            // name: response.data.xxxx,
+            image: response.data.hits[10].largeImageURL
           })
       })
   }
@@ -38,27 +38,36 @@ export default class CityInfo extends Component {
     return(
 
         <div>
-            <div name="Name">
-              <h1>City Name: {this.state.name}</h1>
-            </div>
-
+            {/* <div name="Name">
+              { <h1>City Name: {this.state.name}</h1> }
+            </div> */}
+            {/* {console.log(this.state.image)} */}
+            <img src={this.state.image} />
             {/* Links part in untiled file */}
+              <Router>
+                <Link to="/Info">Information</Link>
+                {'  '}
+                <Link to="/Places">Places</Link> 
+                {'  '}
+                {/* Determine what places should show.. Rest. or places.. */}
+                <Link to="/Places">Restaurants</Link>
+              </Router>
 
             <div name="Info">
-              <Info />
+              {/* <Info /> */}
             </div>
             
             <div name="TourismPlaces">
-              <Places /> {/* Specify that it's places */}
+              {/* <Places /> Specify that it's places */}
             </div>
 
             <div name="Rest">
               <h1>Restaurants</h1>
               {/* Specify that it's rest. and change by if condition the content of header*/}
-              <Places /> 
+              {/* <Places />  */}
             </div>
 
-            <CityImage />
+            {/* <CityImage /> */}
 
 
 
