25861e74a8665f3de200febb33ed2d57f8ac03e0
Ahmed <sport996@github.com>
2019-08-20 02:32:51 +0300

    test api

--- tourism-project/src/AllVideos.js
+++ tourism-project/src/AllVideos.js
@@ -1,17 +1,14 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 
 export default class AllVideos extends Component{
 
 render(){
-    console.log(this.props.task)
     return(
         <div>
             <iframe width="420" height="320"
-            src={this.props.task} >
+            src={this.props.vid} >
             </iframe>
         </div>
-        
     );
     
 }
