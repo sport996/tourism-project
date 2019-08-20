06906999a2ede4babe556393614fe16cc6c3c1f5
ahlamcpp <a7lam.al.sa6am@hotmail.com>
2019-08-20 18:35:40 +0300

    api working

--- tourism-project/src/App.js
+++ tourism-project/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Router, Link } from "@reach/router";
+import { BrowserRouter, Route ,Switch} from 'react-router-dom'
 import NavbarMain from "./Navbar";
 import Home from "./Home";
 import Tour from "./Tour";
@@ -8,15 +9,32 @@ import axios from 'axios';
 
 
 class App extends Component {
-
+  state = {
+    nameCity:[]
+  };
+  componentDidMount() {
+    axios ({
+      method: "GET",
+      url: "https://cors-anywhere.herokuapp.com/https://tourism-project-sei.herokuapp.com/cities.json"
+    })
+    
+    .then(response => { 
+        this.setState({
+          nameCity: response.data
+        })
+  }).catch(arr=> {
+    console.log(arr)
+  })
+  }
   render() {
     return (
       <div className="App">
         <NavbarMain />
-        <Router>
-          <Home path="/" />
-          <Tour path="tour" />
-        </Router>
+        <BrowserRouter>
+        
+          <Route path="/tour/:id"  render={(props) => <Tour {...props} nameCity={this.state.nameCity} />}/>
+          <Route path="/" exact render={(props) => <Home {...props} nameCity={this.state.nameCity} />}/>
+        </BrowserRouter>
       </div>
     );
   }
--- tourism-project/src/Package.js
+++ tourism-project/src/Package.js
@@ -12,88 +12,8 @@ import imgCard7 from "./img/img-card (7).jpg";
 import imgCard8 from "./img/img-card (8).jpg";
 import imgCard9 from "./img/img-card (9).jpg";
 import imgBarobaybay from "./img/barobaybay.jpg";
-const tours = [
-  {
-    id: 1,
-    category: ["Mountain"],
-    img: imgCard1,
-    alt: "blah blah",
-    title: "Ala'a",
-    subtitle: "Ala'a Mountan"
-  },
-  {
-    id: 2,
-    category: ["Sea"],
-    img: imgCard2,
-    alt: "blah blah",
-    title: "Jeddah",
-    subtitle: "The read sea"
-  },
-  {
-    id: 3,
-    category: ["Desert"],
-    img: imgCard3,
-    alt: "blah blah",
-    title: "Raydh",
-    subtitle: "The Capital of Saudi Arabia"
-  },
-  {
-    id: 4,
-    category: ["Sea"],
-    img: imgCard4,
-    alt: "blah blah",
-    title: "Dammam",
-    subtitle: ""
-  },
-  {
-    id: 5,
-    category: ["Mountain"],
-    img: imgCard5,
-    alt: "blah blah",
-    title: "Abaha",
-    subtitle: ""
-  },
-  {
-    id: 6,
-    category: ["Mountain"],
-    img: imgCard6,
-    alt: "blah blah",
-    title: "Taif",
-    subtitle: ""
-  },
-  {
-    id: 7,
-    category: ["Mountain"],
-    img: imgCard7,
-    alt: "blah blah",
-    title: "Asear",
-    subtitle: ""
-  },
-  {
-    id: 8,
-    category: ["Desert"],
-    img: imgCard8,
-    alt: "blah blah",
-    title: "Alahsa",
-    subtitle: ""
-  },
-  {
-    id: 9,
-    category: ["Desert"],
-    img: imgCard9,
-    alt: "blah blah",
-    title: "Hofof",
-    subtitle: ""
-  },
-  {
-    id: 10,
-    category: ["Mountain"],
-    img: imgBarobaybay,
-    alt: "camping in the lake",
-    title: "Alqar Mountan",
-    subtitle: ""
-  }
-];
+import axios from "axios";
+
 
 const itemCategories = [
   "All",
@@ -106,14 +26,40 @@ const itemCategories = [
 class Package extends Component {
   state = {
     cards: [],
-    category: "All"
+    category: "All",
+    tours:[],
+    nameCity:[]
   };
 
   componentDidMount() {
-    this.setState({ cards: tours });
+    axios ({
+      method: "GET",
+      url: "https://cors-anywhere.herokuapp.com/https://tourism-project-sei.herokuapp.com/cities.json"
+    })
+    
+    .then(response => { 
+
+      // console.log(response)
+      // let city = this.state.nameCity
+      // city.push(response.data[i].name)
+        this.setState({
+          // nameCity: response.data.name
+          nameCity: response.data
+        })
+        // console.log("**",response.data)
+  }).catch(arr=> {
+    console.log(arr)
+  })
   }
 
+    // this.setState({ 
+    //   cards: tours
+    //  });
+  
+
   render() {
+        console.log(this.state.nameCity)
+
     const { cards, category } = this.state;
     return (
       <div className="subComponent-lg" id="packageBody">
@@ -137,14 +83,12 @@ class Package extends Component {
             <Row className="text-left">
               <CardColumns>
                 {category !== "All"
-                  ? cards.map(tourcard => {
-                      return tourcard.category.map(catItem => {
-                        return catItem === category ? (
-                          <TourCard key={tourcard.id} tourcard={tourcard} />
+                  ? this.props.nameCity.map(tourcard => {
+                        return tourcard.category === category ? (
+                          <TourCard key={tourcard.id} tourcard={tourcard}  />
                         ) : null;
-                      });
                     })
-                  : cards.map(tourcard => (
+                  : this.props.nameCity.map(tourcard => (
                       <TourCard key={tourcard.id} tourcard={tourcard} />
                     ))}
               </CardColumns>
