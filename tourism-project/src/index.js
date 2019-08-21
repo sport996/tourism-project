03c10688e03e098fa1f9ecbf7256fa388e05d581
Ahmed <sport996@github.com>
2019-08-21 20:45:03 +0300

    updated route

--- tourism-project/src/index.js
+++ tourism-project/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { HashRouter} from 'react-router-dom'
+
 import registerServiceWorker from "./registerServiceWorker";
 
 import "bootstrap/dist/css/bootstrap.css";
