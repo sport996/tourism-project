61fb84a80a99a69facd299e497da7234f7b2c063
Ahmed <sport996@github.com>
2019-08-21 20:57:30 +0300

    updated route

--- tourism-project/src/index.js
+++ tourism-project/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Router, Route} from 'react-router-dom'
+import { HashRouter as Router, Route} from 'react-router-dom'
 
 import registerServiceWorker from "./registerServiceWorker";
 
