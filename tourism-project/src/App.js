68910a9dcaefdee5645fc1d2bca0ff60edd82bc0
Ahmed <sport996@github.com>
2019-09-14 22:03:41 +0300

    Adding work & Readme: Add Dep. link

--- tourism-project/src/App.js
+++ tourism-project/src/App.js
@@ -42,10 +42,10 @@ class App extends Component {
         
         <NavbarMain />
         <HashRouter>
-          <Switch>
+          {/* <Switch> */}
           <Route path="/:id"  component={(props) => <Tour {...props} images={this.state.images} nameCity={this.state.nameCity} />}/>
           <Route path="/" exact component={(props) => <Home {...props} nameCity={this.state.nameCity} />}/>
-          </Switch>
+          {/* </Switch> */}
         </HashRouter>
        
       </div>
