d6848fdca8c6c14b5a12ec328a2a60951c353cc2
Ahmed <sport996@github.com>
2019-10-11 18:33:00 +0300

    Add logo update

--- tourism-project/src/App.js
+++ tourism-project/src/App.js
@@ -42,10 +42,8 @@ class App extends Component {
         
         <NavbarMain />
         <HashRouter>
-          {/* <Switch> */}
           <Route path="/:id"  component={(props) => <Tour {...props} images={this.state.images} nameCity={this.state.nameCity} />}/>
           <Route path="/" exact component={(props) => <Home {...props} nameCity={this.state.nameCity} />}/>
-          {/* </Switch> */}
         </HashRouter>
        
       </div>
